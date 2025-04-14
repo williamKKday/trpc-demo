c316e34d11698ff6642bd7c99e4e176780e69aba
william.lin <william.lin@kkday.com>
2025-04-14 23:39:43 +0800

    feat: logger

--- trpc/client.ts
+++ trpc/client.ts
@@ -1,9 +1,16 @@
 import type { AppRouter } from "@/trpc/index";
 
-import { createTRPCClient, httpBatchLink } from "@trpc/client";
+import { createTRPCClient, httpBatchLink, loggerLink } from "@trpc/client";
 
 const client = createTRPCClient<AppRouter>({
   links: [
+    /**
+     * The function passed to enabled is an example in case you want to the link to
+     * log to your console in development and only log errors in production
+     */
+    loggerLink({
+      enabled: () => process.env.NODE_ENV === "development",
+    }),
     httpBatchLink({
       url: `http://localhost:3000/api/trpc`,
     }),
